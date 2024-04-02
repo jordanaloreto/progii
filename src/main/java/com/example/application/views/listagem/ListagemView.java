e4d6a200a001ba70054c51c89335dcfa2445d3b3
Jordana Loreto <jordanaloreto45@gmail.com>
2024-04-02 16:30:29 -0300

    ajute

--- src/main/java/com/example/application/views/listagem/ListagemView.java
+++ src/main/java/com/example/application/views/listagem/ListagemView.java
@@ -1,73 +1,106 @@
 package com.example.application.views.listagem;
 
+import com.example.application.data.Livro;
 import com.example.application.data.SamplePerson;
 import com.example.application.services.SamplePersonService;
-import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.grid.editor.Editor;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+import com.vaadin.flow.data.binder.ValidationResult;
 
-@PageTitle("Listagem")
-@Route(value = "Listagem", layout = MainLayout.class)
-@Uses(Icon.class)
-public class ListagemView extends Composite<VerticalLayout> {
+
+import java.util.List;
+
+@Route("grid-buffered-inline-editor")
+public class ListagemView extends VerticalLayout {
 
     public ListagemView() {
-        FormLayout formLayout2Col = new FormLayout();
-        Select select = new Select();
-        TextField textField = new TextField();
-        Grid stripedGrid = new Grid(SamplePerson.class);
-        getContent().setWidth("100%");
-        getContent().getStyle().set("flex-grow", "1");
-        formLayout2Col.setWidth("100%");
-        select.setLabel("Selecione o quer pesquisar");
-        select.setWidth("min-content");
-        setSelectSampleData(select);
-        textField.setLabel("Nome");
-        textField.setWidth("min-content");
-        stripedGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
-        stripedGrid.setWidth("100%");
-        stripedGrid.getStyle().set("flex-grow", "0");
-        setGridSampleData(stripedGrid);
-        getContent().add(formLayout2Col);
-        formLayout2Col.add(select);
-        formLayout2Col.add(textField);
-        getContent().add(stripedGrid);
+        ValidationMessage firstNameValidationMessage = new ValidationMessage();
+        ValidationMessage lastNameValidationMessage = new ValidationMessage();
+        ValidationMessage emailValidationMessage = new ValidationMessage();
+
+        // tag::snippet[]
+        Grid<Livro> grid = new Grid<>(Livro.class, false);
+        Editor<Livro> editor = grid.getEditor();
+
+        Grid.Column<Livro> firstNameColumn = grid
+                .addColumn(Livro::getNomeLivro).setHeader("First name")
+                .setWidth("120px").setFlexGrow(0);
+        Grid.Column<Livro> lastNameColumn = grid.addColumn(Livro::getAutor)
+                .setHeader("Last name").setWidth("120px").setFlexGrow(0);
+        Grid.Column<Livro> emailColumn = grid.addColumn(Livro::getEditora)
+                .setHeader("Email");
+        Grid.Column<Livro> editColumn = grid.addComponentColumn(person -> {
+            Button editButton = new Button("Edit");
+            editButton.addClickListener(e -> {
+                if (editor.isOpen())
+                    editor.cancel();
+                grid.getEditor().editItem(person);
+            });
+            return editButton;
+        }).setWidth("150px").setFlexGrow(0);
+
+        Binder<Livro> binder = new Binder<>(Livro.class);
+        editor.setBinder(binder);
+        editor.setBuffered(true);
+
+        TextField firstNameField = new TextField();
+        firstNameField.setWidthFull();
+        binder.forField(firstNameField)
+                .asRequired("First name must not be empty")
+                .withStatusLabel(firstNameValidationMessage)
+                .bind(Livro::getNomeLivro, Livro::setNomeLivro);
+        firstNameColumn.setEditorComponent(firstNameField);
+
+        TextField lastNameField = new TextField();
+        lastNameField.setWidthFull();
+        binder.forField(lastNameField).asRequired("Last name must not be empty")
+                .withStatusLabel(lastNameValidationMessage)
+                .bind(Livro::getAutor, Livro::setAutor);
+        lastNameColumn.setEditorComponent(lastNameField);
+
+        EmailField emailField = new EmailField();
+        emailField.setWidthFull();
+        binder.forField(emailField).asRequired("Email must not be empty")
+                .withValidator(
+                        new EmailValidator("Enter a valid email address"))
+                .withStatusLabel(emailValidationMessage)
+                .bind(Livro::getEditora, Livro::setEditora);
+        emailColumn.setEditorComponent(emailField);
+
+        Button saveButton = new Button("Save", e -> editor.save());
+        Button cancelButton = new Button(VaadinIcon.CLOSE.create(),
+                e -> editor.cancel());
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_ICON,
+                ButtonVariant.LUMO_ERROR);
+        HorizontalLayout actions = new HorizontalLayout(saveButton,
+                cancelButton);
+        actions.setPadding(false);
+        editColumn.setEditorComponent(actions);
+        // end::snippet[]
+
+        editor.addCancelListener(e -> {
+            firstNameValidationMessage.setText("");
+            lastNameValidationMessage.setText("");
+            emailValidationMessage.setText("");
+        });
+
+        List<Livro> people = DataService.getPeople();
+        grid.setItems(people);
+
+        getThemeList().clear();
+        getThemeList().add("spacing-s");
+        add(grid, firstNameValidationMessage, lastNameValidationMessage,
+                emailValidationMessage);
     }
 
-    record SampleItem(String value, String label, Boolean disabled) {
-    }
-
-    private void setSelectSampleData(Select select) {
-        List<SampleItem> sampleItems = new ArrayList<>();
-        sampleItems.add(new SampleItem("first", "First", null));
-        sampleItems.add(new SampleItem("second", "Second", null));
-        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
-        sampleItems.add(new SampleItem("fourth", "Fourth", null));
-        select.setItems(sampleItems);
-        select.setItemLabelGenerator(item -> ((SampleItem) item).label());
-        select.setItemEnabledProvider(item -> !Boolean.TRUE.equals(((SampleItem) item).disabled()));
-    }
-
-    private void setGridSampleData(Grid grid) {
-        grid.setItems(query -> samplePersonService.list(
-                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
-                .stream());
-    }
-
-    @Autowired()
-    private SamplePersonService samplePersonService;
 }
